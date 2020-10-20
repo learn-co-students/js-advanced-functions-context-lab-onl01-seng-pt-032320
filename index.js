5d0bc95d2361b8a35d73d5c3de69bb4914fa1297
N-Santiago <norbertosantiago82@gmail.com>
2020-10-19 21:59:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
 /* Your Code Here */
+const createEmployeeRecord = (empArray) => {
+    return {
+        firstName: empArray[0],
+        familyName: empArray[1],
+        title: empArray[2],
+        payPerHour: empArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (empArrays) => {
+    return empArrays.map(createEmployeeRecord)
+}
+
+const createDateOBJ = (getType, dateTime) => {
+    return {type: getType, date: dateTime.slice(0, 10), hour: parseInt(dateTime.slice(-4))}
+}
+
+const createTimeInEvent = (employee, dateTime) => {
+    employee.timeInEvents.push(createDateOBJ("TimeIn", dateTime))
+    return employee
+}
+
+const createTimeOutEvent = (employee, dateTime) => {
+    employee.timeOutEvents.push(createDateOBJ("TimeOut", dateTime))
+    return employee
+}
+
+const hoursWorkedOnDate = (employee, dateYMD) => {
+    const timeIn = employee.timeInEvents.find((e) => e.date === dateYMD).hour
+    const timeOut = employee.timeOutEvents.find((e) => e.date === dateYMD).hour
+    return (timeOut - timeIn)/100
+}
+
+const wagesEarnedOnDate = (employee, dateYMD) => {
+    const rawWage = hoursWorkedOnDate(employee, dateYMD) * employee.payPerHour
+    return rawWage 
+}
+
+const calculatePayroll = (records) => {
+    return records.reduce((memo, rec) => {
+        return memo + allWagesFor(rec)
+    }, 0)
+}
+
+const findEmployeeByFirstName = (srcArray, firstName) => {
+    return srcArray.find((rec) => {
+        return rec.firstName === firstName
+    })
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
