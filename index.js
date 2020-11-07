0ea31cec1d6570f64a19ee02d787bdf6118512f5
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-11-06 21:01:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,108 @@
 /* Your Code Here */
+let createEmployeeRecords = function(employeeData) {
+    return employeeData.map(function(eachrow){
+        return createEmployeeRecord(eachrow)
+    })
+}
+
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createTimeInEvent = function(datetime){
+    let [date, hour] = datetime.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+
+// let addFive = function(){
+//    return this + 5
+//}
+//You'd call this by writing number.addFive like 7.addFive
+
+
+
+// let addFive = function(number){
+// return number + 5
+// }s
+// You call this by writing addFive(7)
+
+
+// let createTimeInEvent = function(employee, dateStamp){
+//     let [date, hour] = dateStamp.split(' ')
+
+//     employee.timeInEvents.push({
+//         type: "TimeIn",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+
+//     return employee
+// }
+
+let newArray = []
+newArray.createTimeInEvent(dateStamp)
+let newString = ""
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(soughtDate){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+
+
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
 
 /*
+
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
@@ -8,6 +110,7 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
